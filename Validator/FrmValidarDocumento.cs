ece8e5e23d21edecc2c3d2786d21271bd874b839
Sixto Santiago Aisa <santiagoaisa@gmail.com>
2021-10-13 16:06:24 -0500

    [FIX] JUSTES CODIGO DE ERROR

--- Validator/FrmValidarDocumento.cs
+++ Validator/FrmValidarDocumento.cs
@@ -24,19 +24,19 @@ namespace Validator
 
         private void llenarData()
         {            
-            dataGrid.Rows.Add("FA", "F001", "011661", "2021.09.28", "LDM, PERU", 511.00, "XML", "-", "Enviar");
+            dataGrid.Rows.Add("FA", "F001", "011661", "2021.09.28", "LDM, PERU", 511.00, "XML", "301", "Enviar");
             dataGrid.Rows.Add("FA", "F001", "011663", "2021.09.28", "BOTICA ROUSS FARMA", 1015.00, "XML", "-", "Enviar");
-            dataGrid.Rows.Add("FA", "F001", "011666", "2021.09.28", "BOTICA ANY FARMA", 196.00, "XML", "-", "Enviar");
+            dataGrid.Rows.Add("FA", "F001", "011666", "2021.09.28", "BOTICA ANY FARMA", 196.00, "XML", "302", "Enviar");
             dataGrid.Rows.Add("FA", "F001", "011668", "2021.09.28", "NEGOCIOS GENERALES ROSYMAR E.I.R.L.", 316.00, "XML", "-", "Enviar");
             dataGrid.Rows.Add("FA", "F001", "011671", "2021.09.28", "BOTICA ROUSS FARMA E.I.R.L.", 354.00, "XML", "-", "Enviar");
             dataGrid.Rows.Add("FA", "F001", "011672", "2021.09.28", "LI FARMA.", 24.00, "XML", "-", "Enviar");
             dataGrid.Rows.Add("FA", "F001", "011673", "2021.09.28", "BOTICA FAMILIA & SALUD", 28.90, "XML", "-", "Enviar");
-            dataGrid.Rows.Add("FA", "F001", "011675", "2021.09.28", "MERLIN DORADO S.A.C.", 478.40, "XML", "-", "Enviar");
+            dataGrid.Rows.Add("FA", "F001", "011675", "2021.09.28", "MERLIN DORADO S.A.C.", 478.40, "XML", "302", "Enviar");
             dataGrid.Rows.Add("FA", "F001", "011677", "2021.09.29", "NILDA ELICIA LEVITA MAMANI", 451.10, "XML", "-", "Enviar");
             dataGrid.Rows.Add("FA", "F001", "011678", "2021.09.29", "AUTOSERVICIO LA GRANJITA SAC", 60.40, "XML", "-", "Enviar");
             dataGrid.Rows.Add("FA", "F001", "011679", "2021.09.29", "AUTOSERVICIO LA GRANJITA SAC", 1109.10, "XML", "-", "Enviar");
             dataGrid.Rows.Add("FA", "F001", "011683", "2021.09.29", "BOTICA EKO.S FARMA", 278.60, "XML", "-", "Enviar");
-            dataGrid.Rows.Add("FA", "F001", "011690", "2021.09.29", "BOTICA MIA FARMA", 20.10, "XML", "-", "Enviar");
+            dataGrid.Rows.Add("FA", "F001", "011690", "2021.09.29", "BOTICA MIA FARMA", 20.10, "XML", "302", "Enviar");
             dataGrid.Rows.Add("FA", "F001", "011691", "2021.09.29", "BOTICA MIA FARMA", 87.10, "XML", "-", "Enviar");
             dataGrid.Rows.Add("FA", "F001", "011692", "2021.09.29", "BOTICA EL PUEBLO II", 280.20, "XML", "-", "Enviar");
             dataGrid.Rows.Add("FA", "F001", "011693", "2021.09.29", "GRUPO GUICAR S.A.C.", 1185.70, "XML", "-", "Enviar");
